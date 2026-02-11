888ba0b4ff1ad46f917d3f945ba614348ab1f7ff
kiztbz <pragyapravita@gmail.com>
2026-02-11 19:15:27 +0530

    updating db preferences

--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ app.use(express.json());
 mongoose.connect("mongodb+srv://scotty:scotty123@cluster0.ahe7o9c.mongodb.net/?appName=Cluster0", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+    dbName: "test",
 });
 
 app.get("/", (req,res)=>res.send("API running"));
